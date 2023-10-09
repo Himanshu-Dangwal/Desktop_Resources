3ca2ed673ba70515b9ec76080a97257048c09254
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2023-10-09 13:01:51 +0530

    Some changes in Fenwick Tree under Advanced DSA

--- DSA/Advanced Data Structures/Binary Lifting in fenwick Trees.cpp
+++ DSA/Advanced Data Structures/Binary Lifting in fenwick Trees.cpp
@@ -1,6 +1,6 @@
-//When we need to find the lower bound for any sum given:::
-//Fenwick Tree to find sum over a range::
-#include<bits/stdc++.h>
+// When we need to find the lower bound for any sum given:::
+// Fenwick Tree to find sum over a range::
+#include <bits/stdc++.h>
 using namespace std;
 #define endl "\n"
 #define MOD 1000000007
@@ -8,21 +8,25 @@ typedef long long ll;
 
 int fen[10000];
 
+void update(int idx, int val, int n)
+{
 
-void update(int idx,int val,n){
-	
-	while(idx <= n){
+	while (idx <= n)
+	{
 		fen[idx] += val;
 		idx += idx & (-idx);
 	}
 }
 
-int query(int sum,int n){
-	
-	int curr = 0,ans = 0,prevSum=0;
-	
-	for(int i=log2(n);i>=0;i--){
-		if(fen[curr + (1<<i)] + prevSum < sum){
+int query(int sum, int n)
+{
+
+	int curr = 0, ans = 0, prevSum = 0;
+
+	for (int i = log2(n); i >= 0; i--)
+	{
+		if (fen[curr + (1 << i)] + prevSum < sum)
+		{
 			curr += (1 << i);
 			prevSum += fen[curr];
 		}
@@ -30,24 +34,24 @@ int query(int sum,int n){
 	return curr + 1;
 }
 
-
-int main(){
+int main()
+{
 	int n;
-	cin>>n;
-	
+	cin >> n;
+
 	int arr[n];
 	int idx = 1;
-	for(int i=0;i<n;i++){
-		cin>>arr[i];
-		update(idx,arr[i],n+1);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+		update(idx, arr[i], n + 1);
 		idx++;
 	}
-	
+
 	int sum;
-	cin>>sum;
-	
-	int lowerBound = query(sum,n);
+	cin >> sum;
+
+	int lowerBound = query(sum, n);
 
 	return 0;
 }
-
--- DSA/Arrays/Any base to any base.cpp
+++ DSA/Arrays/Any base to any base.cpp
@@ -1,50 +1,50 @@
-//Any base to any base::
-#include<bits/stdc++.h>
+// Any base to any base::
+#include <bits/stdc++.h>
 using namespace std;
 #define endl "\n"
 #define MOD 1000000007
 typedef long long ll;
 
-int main(){
+int main()
+{
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	
+
 	int n;
-	cin>>n;
-	
+	cin >> n;
+
 	int base1;
-	cin>>base1;
-	
+	cin >> base1;
+
 	int base2;
-	cin>>base2;
-	
-	//base1 to decimal::
+	cin >> base2;
+
+	// base1 to decimal::
 
 	int p = 1;
 	int ans1 = 0;
-	
-	while(n != 0){
-		int rem = n%10;
+
+	while (n != 0)
+	{
+		int rem = n % 10;
 		ans1 += rem * p;
-		p = p*base1;
+		p = p * base1;
 		n /= 10;
 	}
-	cout<<"In decimal = "<<ans1<<endl;
-	//1011 in base 2 -> base 8::
-	//decimal to base2
-	p=1;
+	cout << "In decimal = " << ans1 << endl;
+	// 1011 in base 2 -> base 8::
+	// decimal to base2
+	p = 1;
 	int ans2 = 0;
-	
-	while(ans1 != 0){
-		int rem = ans1%base2;
+
+	while (ans1 != 0)
+	{
+		int rem = ans1 % base2;
 		ans2 += rem * p;
-		p = p*10;
-		ans1 /= 8;
+		p = p * 10;
+		ans1 /= base2;
 	}
-	
-	cout<<ans2<<endl;
+
+	cout << ans2 << endl;
 	return 0;
 }
-
-
-
