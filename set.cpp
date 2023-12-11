c4fa8439e5f76a37bf3997aa4bff505b2842ab80
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2023-12-11 12:08:28 +0530

    backtracking and DP

--- set.cpp
+++ set.cpp
@@ -1,15 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution
+{
+public:
+    int ans = 0;
+    void solve(int i, vector<int> &arr, int target)
+    {
+        if (target == 0)
+        {
+            ans++;
+            return;
+        }
+        if (target < 0)
+            return;
+        if (i == arr.size())
+            return;
+        solve(i + 1, arr, target);
+        solve(i, arr, target - arr[i]);
+    }
+    int combinationSum(vector<int> &arr, int target)
+    {
+        solve(0, arr, target);
+        return ans;
+    }
+};
+
 int main()
 {
-    unordered_set<int> st;
-    st.insert(1);
-    st.insert(2);
-    st.insert(3);
-
-    for (auto it : st)
-    {
-        cout << it << " ";
-    }
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    Solution ans;
+    int k;
+    cin >> k;
+    cout << ans.combinationSum(a, k) << endl;
+    return 0;
 }
