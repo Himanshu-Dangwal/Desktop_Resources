1a8afb347ec0090b82433fb9a628ecf274397020
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-04-16 14:36:13 +0530

    Pursuit to happiness

--- CV Raman Contest/Q2/solution.cpp
+++ CV Raman Contest/Q2/solution.cpp
@@ -1,17 +1,21 @@
 //[C++]
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
     long long l;
     long long r;
-    cin>>l>>r;
-    if(l%2==0) l++;
-    if(r%2==0) r--;
-  
-    int no_of_odd_nos=((r-l)/2)+1;
-    if(no_of_odd_nos%2){
-        cout<<"odd\n";
-    }
-    else cout<<"even\n";
+    cin >> l >> r;
+    if (l % 2 == 0)
+        l++;
+    if (r % 2 == 0)
+        r--;
 
+    int no_of_odd_nos = ((r - l) / 2) + 1;
+    if (no_of_odd_nos % 2)
+    {
+        cout << "odd\n";
+    }
+    else
+        cout << "even\n";
 }
