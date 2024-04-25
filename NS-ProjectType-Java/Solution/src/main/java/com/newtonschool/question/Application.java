d8f1c1c00c9067adf41f6ff05218a1e223951391
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-04-25 19:43:49 +0530

    Solution - project type java added

---
+++ NS-ProjectType-Java/Solution/src/main/java/com/newtonschool/question/Application.java
@@ -0,0 +1,29 @@
+package com.newtonschool.question;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        readFromFile("dummy.txt");
+    }
+
+    public static void readFromFile(String fileName) {
+        // Load file from resources folder using classloader
+        InputStream inputStream = Application.class.getClassLoader().getResourceAsStream(fileName);
+        if (inputStream == null) {
+            System.out.println("File not found: " + fileName);
+            return;
+        }
+
+        try (Scanner scanner = new Scanner(inputStream)) {
+            while (scanner.hasNextLine()) {
+                String data = scanner.nextLine();
+                System.out.println(data);
+            }
+        } catch (Exception e) {
+            System.out.println("An error has occurred.");
+            e.printStackTrace();
+        }
+    }
+}
