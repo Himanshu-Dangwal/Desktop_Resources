5a29ebdf4e9ace6874af71ce68e8f21bcf0a0bbd
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-04-25 19:43:29 +0530

    Boilerplate added

---
+++ NS-ProjectType-Java/Boilerplate/src/main/java/com/newtonchool/question/Application.java
@@ -0,0 +1,11 @@
+package com.newtonchool.question;
+
+public class Application {
+    public static void main(String[] args) {
+        readFromFile("dummy.txt");
+    }
+
+    public static void readFromFile(String fileName) {
+        // you code goes here
+    }
+}
