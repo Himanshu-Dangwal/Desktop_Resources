fb21e46b5c6f1fd43fa5dbc211b568f864ff2fa3
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-08-20 20:23:23 -0700

    Updated test.py

--- test.py
+++ test.py
@@ -1,16 +1,22 @@
-def funcA(n):
-    if n == 0:
-        return
-    else:
-        print(n, end=' ')
-        funcB(n-1)
+# def funcA(n):
+#     if n == 0:
+#         return
+#     else:
+#         print(n, end=' ')
+#         funcB(n-1)
 
-def funcB(n):
-    if n == 1 or n == 0:
-        print(n, end=' ')
-        return
-    else:
-        print(n, end=' ')
-        funcA(n//2)
+# def funcB(n):
+#     if n == 1 or n == 0:
+#         print(n, end=' ')
+#         return
+#     else:
+#         print(n, end=' ')
+#         funcA(n//2)
 
-funcA(10)
+# funcA(10)
+
+x = input()
+print(x*3)
+
+y = list(map(int, input().split()))
+print(y)
