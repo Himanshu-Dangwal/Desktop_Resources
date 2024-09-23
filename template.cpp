349bf5d5974d1ed97cef7ff65363edd60aa85ea9
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-09-23 13:53:13 -0700

    Snippet and template updated

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define MOD 1000000007
+typedef long long ll;
+
+void solve()
+{
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+}
---
+++ testSnippet.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define MOD 1000000007
+typedef long long ll;
+
+void solve()
+{
+    // Cursor should be here
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
