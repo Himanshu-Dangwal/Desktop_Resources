99a23b9759be2a04bd78f0521aceffc3dc6f73c5
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-10-12 15:04:11 -0700

    a

--- Coding Ninjas/Number Theory3/Modified Power Function.cpp
+++ Coding Ninjas/Number Theory3/Modified Power Function.cpp
@@ -1,33 +1,39 @@
-//Modified Power Function
+// Modified Power Function
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
 #define MOD 1000000007
 
-ll power(ll x,ll y){
-	if(x == 0){
+ll power(ll x, ll y)
+{
+	if (x == 0)
+	{
 		return 0;
 	}
-	if(y == 0){
+	if (y == 0)
+	{
 		return 1;
 	}
 	ll ans;
-	if(y%2 == 0){
-		ll val = power(x,y/2);
-		ans =  ((val)%MOD*(val)%MOD)%MOD;
+	if (y % 2 == 0)
+	{
+		ll val = power(x, y / 2);
+		ans = ((val) % MOD * (val) % MOD) % MOD;
 	}
-	else{
-		ll val = power(x,y/2);
-		ans = ((val)%MOD*(val)%MOD*(x)%MOD)%MOD;
+	else
+	{
+		ll val = power(x, y / 2);
+		ans = ((val) % MOD * (val) % MOD * (x) % MOD) % MOD;
 	}
-	return ans;;
+	return ans;
 }
 
-int main(){
-	ll x,y;
-	cin>>x>>y;
-	//To find power(x,y) i.e  x^y ::
-	cout<<power(x,y);
+int main()
+{
+	ll x, y;
+	cin >> x >> y;
+	// To find power(x,y) i.e  x^y ::
+	cout << power(x, y);
 	return 0;
 }
