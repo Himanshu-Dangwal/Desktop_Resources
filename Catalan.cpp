fec82fb12e79a19da35e4ec5e5e80176824a346b
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-10-12 17:56:15 -0700

    Catalan Number

---
+++ Catalan.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define MOD 1000000007
+typedef long long ll;
+
+int Catalan(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+    vector<int> dp(n + 1, 0);
+    dp[0] = dp[1] = 1;
+
+    for (int i = 2; i <= n; i++)
+    {
+        int ans = 0;
+        int l = 0, r = i - 1;
+        while (r >= 0)
+        {
+            ans += dp[l] * dp[r];
+            l++;
+            r--;
+        }
+        dp[i] = ans;
+    }
+
+    return dp[n];
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    cout << Catalan(n);
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
