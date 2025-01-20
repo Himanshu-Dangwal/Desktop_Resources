c0ce0fc5428024f59a95030f4b0d9c0c1409e6ab
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2025-01-20 20:38:54 +0530

    Finding Set Count (Hashing)

--- CSES_ALL/AmbitiousKid.cpp
+++ CSES_ALL/AmbitiousKid.cpp
@@ -27,3 +27,64 @@ int main()
     cout << ans;
     return 0;
 }
+
+
+//FInding Set Count (Hashing)
+
+#define pii pair<int,int>
+#define f first
+#define s second
+class Solution {
+public:
+    int n, m;
+    bool check(vector<vector<int>>& mat, int x, int y) {
+        bool check = true;
+        //Check row
+        for (int j = 0;j < m;j++) {
+            if (mat[x][j] != 0) {
+                check = false;
+                break;
+            }
+        }
+
+        if (check) return true;
+        check = true;
+        // cout<<"HEre"<<endl;
+        //Check col
+        for (int i = 0;i < n;i++) {
+            if (mat[i][y] != 0) {
+                check = false;
+                break;
+            }
+        }
+        if (check) return true;
+        return false;
+    }
+
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+
+        n = mat.size(), m = mat[0].size();
+        map<int, pii> mp;
+
+        for (int i = 0;i < n;i++) {
+            for (int j = 0;j < m;j++) {
+                mp[mat[i][j]] = { i,j };
+            }
+        }
+
+        vector<int> setCountX(n, 0), setCountY(m, 0);
+
+        for (int i = 0;i < arr.size();i++) {
+            int x = mp[arr[i]].f, y = mp[arr[i]].s;
+            // cout<<x<<" "<<y<<endl;
+            // mat[x][y] = 0;
+            // if(check(mat,x,y)) return i;
+            setCountX[x]++;
+            setCountY[y]++;
+            // cout<<setCountX[x]<<" "<<setCountX[y]<<endl;
+            if ((setCountX[x] == (m)) || (setCountY[y] == (n))) return i;
+        }
+
+        return -1;
+    }
+};
