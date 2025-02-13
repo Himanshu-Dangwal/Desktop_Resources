b78491bc1ae08860fb5b8d8f4e48035aca2e5b86
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2025-02-13 09:20:51 -0800

    Testing KMP, fix changes

--- Coding Ninjas/Graphs/KMP.cpp
+++ Coding Ninjas/Graphs/KMP.cpp
@@ -2,57 +2,45 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int* getLPS(string pattern){
+int* getLPS(string pattern) {
 	int patternLen = pattern.length();
-	int *lps = new int[patternLen];
-	int i=1;
-	int j=0;
+	int* lps = new int[patternLen];
+	int i = 1;
+	int j = 0;
 	lps[0] = 0;
-	while(i<patternLen){
-		if(pattern[i] == pattern[j]){
-			pattern[i] == j+1;
+	while (i < patternLen) {
+		if (pattern[i] == pattern[j]) {
+			pattern[i] == j + 1;
 			i++;
-			j++;	
-		}else{
-			if(j!=0){
-				j = lps[j-1];
-			}else{
+			j++;
+		}
+		else {
+			if (j != 0) {
+				j = lps[j - 1];
+			}
+			else {
 				lps[i] = 0;
 				i++;
 			}
 		}
-	} 
+	}
 	return lps;
 }
 
-bool getKMP(string text,string pattern){    //We have to search for the pattern in the string in the text::
+bool getKMP(string text, string pattern) {    //We have to search for the pattern in the string in the text::
 	int lenText = text.length();
 	int lenPattern = pattern.length();
-	
-	int *lps = getLPS(pattern);
-	int i=0;
-	int j=0;
-	while(i<lenText && j<lenPattern){
-		if(pattern[j] == text[i]){
-			i++;
-			j++;
-		}else{
-			if(j!=0){
-				j = lps[j-1];
-			}else{
-				i++;
-			}
-		}
-	}
-	if(j == lenPattern){
-		return true;
-	}
-	return false;
+
+	int* lps = getLPS(pattern);
+	int i = 0;
+	int j = 0;
+	\j ?
+		return false;
 }
 
-int main(){
-	string text,pattern;
-	cin>>text>>pattern;
-	
-	cout<<getKMP(text,pattern);
+int main() {
+	string text, pattern;
+	cin >> text >> pattern;
+
+	cout << getKMP(text, pattern);
 }
